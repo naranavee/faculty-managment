fc5976e951c3d1eb3a9903cb20ee9a4f94ff593a
naveen <naranaveen576@gmail.com>
2024-08-27 16:03:10 +0530

    added profile button

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,7 @@ app.use(express.json());
 
 // Routes
 app.use('/api/auth', require('./routes/auth'));
+app.use('/api/faculty', require('./routes/faculty'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
